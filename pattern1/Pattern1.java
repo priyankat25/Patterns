24ee5777bfc7cb3ea4b6d9de97b113675c3f574f
priyankat25 <161025060+priyankat25@users.noreply.github.com>
2024-12-06 12:03:25 +0530

    Add files via upload

---
+++ pattern1/Pattern1.java
@@ -0,0 +1,26 @@
+package com.kodnest.pattern1;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("User! Please enter any integer value except zero and negative values: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<n; i++) {
+			for(int j=1; j<n; j++) {
+				if(j<=i) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
